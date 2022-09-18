fa68f1b632daca5f7819a5c3794db8a048e47069
juruizgi <111092148+juruizgi@users.noreply.github.com>
2022-09-17 19:30:33 -0500

    Update Reptil.java

--- src/main/java/zooAnimales/Reptil.java
+++ src/main/java/zooAnimales/Reptil.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package zooAnimales;
 import java.util.ArrayList;
-/**
- *
- * @author USUARIO
- */
+
 public class Reptil extends Animal{
 
     private static ArrayList<Reptil> listado = new ArrayList<>();
