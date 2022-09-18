258b6e5042535fd27af7a268221ed79eac5e69aa
juruizgi <111092148+juruizgi@users.noreply.github.com>
2022-09-17 19:27:47 -0500

    Update Mamifero.java

--- src/main/java/zooAnimales/Mamifero.java
+++ src/main/java/zooAnimales/Mamifero.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package zooAnimales;
 import java.util.ArrayList;
-/**
- *
- * @author USUARIO
- */
+
 public class Mamifero extends Animal{
     private static ArrayList<Mamifero> listado = new ArrayList<>();
     public static int caballos;
