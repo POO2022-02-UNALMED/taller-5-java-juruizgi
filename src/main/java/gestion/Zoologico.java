5f0d2d63698446660f68b60f2e28b9d6239956cc
juruizgi <111092148+juruizgi@users.noreply.github.com>
2022-09-17 19:23:53 -0500

    Update Zoologico.java

--- src/main/java/gestion/Zoologico.java
+++ src/main/java/gestion/Zoologico.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package gestion;
 import java.util.ArrayList;
-/**
- *
- * @author USUARIO
- */
+
 public class Zoologico {
     private String nombre;
     private String ubicacion;
