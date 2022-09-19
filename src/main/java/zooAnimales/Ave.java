5603af20366c02abb7c5a3409d8284e6a639664d
juruizgi <111092148+juruizgi@users.noreply.github.com>
2022-09-19 13:36:33 -0500

    Update Ave.java

--- src/main/java/zooAnimales/Ave.java
+++ src/main/java/zooAnimales/Ave.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
  *
  * @author USUARIO
  */
-class Ave extends Animal {
+public class Ave extends Animal {
     private static ArrayList<Ave> listado = new ArrayList<>();
     public static int halcones;
     public static int aguilas;
