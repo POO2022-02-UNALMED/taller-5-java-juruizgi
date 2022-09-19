10bfdd367620312a78ab0c97984a7359aa1b712e
juruizgi <111092148+juruizgi@users.noreply.github.com>
2022-09-19 13:14:44 -0500

    Update Mamifero.java

--- src/main/java/zooAnimales/Mamifero.java
+++ src/main/java/zooAnimales/Mamifero.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
  *
  * @author USUARIO
  */
-public class Mamifero extends Animal{
+class Mamifero extends Animal{
     private static ArrayList<Mamifero> listado = new ArrayList<>();
     public static int caballos;
     public static int leones;
