054fff9e2307937b1b45f42617ec5f7371907571
juruizgi <111092148+juruizgi@users.noreply.github.com>
2022-09-19 12:40:11 -0500

    Update Animal.java

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -90,7 +90,7 @@ public class Animal {
     @Override
     public String toString(){
         
-        if (this.zona.isEmpty() || this.getZona().getZoo() == null){
+        if (this.zona.isEmpty() || this.getZona().getZoo() == null || this.getZona() == null){
             return "Mi nombre es "+nombre+", tengo una edad de "+edad+", habito en "+habitat+" y mi genero es "+genero+", la zona en la que me ubico es "+zona.get(0).getNombre()+" en el "+zona.get(0).getZoo().getNombre();
             
         }else{
